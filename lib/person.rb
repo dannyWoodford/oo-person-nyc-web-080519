06847ae1115c12089c269f6dee90e9cccde39e9e
daniel Woodford <daniel.woodford.g@gmail.com>
2019-08-07 14:41:40 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,15 +2,14 @@
 class Person
   
   attr_reader :name
-  attr_accessor :bank_account 
+  attr_accessor :bank_account, :happiness 
   
   def initialize(name)
     @name = name 
     @bank_account =  25
+    @happiness = 8
   end 
   
-  
-   
     
     
 end 
